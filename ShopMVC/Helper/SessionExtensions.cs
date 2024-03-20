a8223044976c890d096301ff9438b526eac2f2f3
gapichigor <guitarigor97@gmail.com>
2024-03-20 20:09:40 +0200

    add test

--- ShopMVC/Helper/SessionExtensions.cs
+++ ShopMVC/Helper/SessionExtensions.cs
@@ -5,12 +5,12 @@ namespace ShopMVC.Helper
 {
     public static class SessionExtensions
     {
-        public static void Set<T>(this ISession session, string key, T value)
+        public static void SetObject<T>(this ISession session, string key, T value)
         {
             session.SetString(key, JsonSerializer.Serialize(value));
         }
 
-        public static T? Get<T>(this ISession session, string key)
+        public static T? GetObject<T>(this ISession session, string key)
         {
             var value = session.GetString(key);
             return value == null ? default : JsonSerializer.Deserialize<T>(value);
